9d17a0b9ff86d146cc90f083325302ffd88e0005
Hubert <jaroszhubert12@gmail.com>
2024-12-08 19:01:41 +0100

    .

--- UkladyCyfrowe/Lab_08/lab_08/lab_08/compile/zad_7.vhd
+++ UkladyCyfrowe/Lab_08/lab_08/lab_08/compile/zad_7.vhd
@@ -7,9 +7,9 @@
 --
 -------------------------------------------------------------------------------
 --
--- File        : C:\Users\Hubert Jarosz\Documents\GitHub\UniwersytetZielonogorski\UkladyCyfrowe\Lab_08\lab_08\lab_08\compile\zad_06.vhd
+-- File        : C:\Users\Hubert Jarosz\Documents\GitHub\UniwersytetZielonogorski\UkladyCyfrowe\Lab_08\lab_08\lab_08\compile\zad_7.vhd
 -- Generated   : Sun Dec  8 18:46:13 2024
--- From        : C:\Users\Hubert Jarosz\Documents\GitHub\UniwersytetZielonogorski\UkladyCyfrowe\Lab_08\lab_08\lab_08\src\zad_6\zad_06.bde
+-- From        : C:\Users\Hubert Jarosz\Documents\GitHub\UniwersytetZielonogorski\UkladyCyfrowe\Lab_08\lab_08\lab_08\src\zad_7\zad_7.bde
 -- By          : Bde2Vhdl ver. 2.6
 --
 -------------------------------------------------------------------------------
@@ -24,44 +24,49 @@ use IEEE.std_logic_arith.all;
 use IEEE.std_logic_signed.all;
 use IEEE.std_logic_unsigned.all;
 
-entity zad_06 is
+entity zad_7 is
   port(
-       Reset : in STD_LOGIC;
        Clk : in STD_LOGIC;
-       y : buffer STD_LOGIC
+       Reset : in STD_LOGIC;
+       Y : out STD_LOGIC
   );
-end zad_06;
+end zad_7;
 
-architecture zad_06 of zad_06 is
+architecture zad_7 of zad_7 is
 
 ---- Component declarations -----
 
-component zad_4
+component zad_5
   port(
        Q : out STD_LOGIC;
        Clk : in STD_LOGIC;
        Reset : in STD_LOGIC;
-       D : in STD_LOGIC
+       J : in STD_LOGIC;
+       K : in STD_LOGIC
   );
 end component;
 
 ---- Signal declarations used on the diagram ----
 
-signal NET676 : STD_LOGIC;
+signal NET712 : STD_LOGIC;
+signal NET740 : STD_LOGIC;
 
 begin
 
 ----  Component instantiations  ----
 
-U1 : zad_4
+U1 : zad_5
   port map(
-       Q => y,
+       Q => NET712,
        Clk => Clk,
        Reset => Reset,
-       D => NET676
+       J => NET740,
+       K => NET740
   );
 
-NET676 <= not(y);
+Y <= NET712;
+
+NET740 <= not(NET712 xor NET712);
 
 
-end zad_06;
+end zad_7;
